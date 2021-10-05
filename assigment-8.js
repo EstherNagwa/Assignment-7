8aa074071b757c439db6d56dfd352ec6b4b3d4b3
Esther Nagwa <esthernagwa7@gmail.com>
2021-10-05 08:25:01 +0300

    javascript

---
+++ assigment-8.js
@@ -0,0 +1,19 @@
+function name(name,age) {
+    console.log("I am called "+name+" and i am "+age+ " years old");
+  }
+  name("Esther",19);
+  name("George",35);
+  name("Parfait",10);
+  name("Greatness",29);
+  name("Mark Angel",41);
+
+let country=20;
+
+for(let  num = 1; num < country; num++){
+    console.log("I LOVE UGANDA")
+}
+let add = (x , y) => x + y;
+
+console.log(add(10, 20));
+
+console.log(typeof add)
